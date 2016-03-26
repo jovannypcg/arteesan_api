48c381e45e060acf2ccb359c7fe39cc83afb50ad
Jovanny Cruz <jovannypcg@yahoo.com>
2016-03-26 17:27:21 +0000

    chore(carts): Change the model

--- models/cart.js
+++ models/cart.js
@@ -5,9 +5,8 @@ let Schema = mongoose.Schema;
 
 let cartSchema = new Schema({
     created     : { type: Date, default: Date.now },
-    owner       : { type: Schema.Types.ObjectId, ref: 'User' },
     products    : [{ type: Schema.Types.ObjectId, ref: 'Product' }],
-    status      : { type: String, default: 'NEUTRAL' } //NEUTRAL, READY, PURCHASED, CANCELED
+    status      : { type: String, default: 'READY' } // READY, PURCHASED, CANCELED
 }, { collection: 'carts' });
 
 let Cart = mongoose.model('Cart', cartSchema);
