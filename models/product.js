5a17118c80152ddbca94456fbd02b0061da1cf0e
Jovanny Cruz <jovannypcg@yahoo.com>
2016-06-14 16:17:27 -0500

    style(products): Align key-values in the model

--- models/product.js
+++ models/product.js
@@ -4,22 +4,22 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let productSchema = new Schema({
-    created_at   : { type: Date, default: Date.now },
+    created_at  : { type: Date, default: Date.now },
     name        : String,
-    price       : Number,
+    price: Number,
     description : { type: String, trim: true },
-    galery      : [{
-        _id : Schema.Types.ObjectId,
-        description: { type: String, trim: true },
-        media_url: String
-    }],
-    status:    : { type: String, default: 'ACTIVE' },
-    available  : { type Boolean, default: true },
+    status      : { type: String, default: 'ACTIVE' },
+    available   : { type: Boolean, default: true },
     tags        : { type: Array, default: [] },
     favorites   : [{ type: Schema.Types.ObjectId, ref: 'User' }],
     purchases   : [{ type: Schema.Types.ObjectId, ref: 'User' }],
     shares      : [{ type: Schema.Types.ObjectId, ref: 'User' }],
-    designer    : { type: Schema.Types.ObjectId, ref: 'User' }
+    designer    : { type: Schema.Types.ObjectId, ref: 'User' },
+    galery      : [{
+        _id: Schema.Types.ObjectId,
+        description: { type: String, trim: true },
+        media_url: String
+    }]
 }, { collection: 'products' });
 
 let Product = mongoose.model('Product', productSchema);
