b207d33da6071b6a58ccf86a1f9c18adbfd2b608
Jovanny Cruz <jovannypcg@yahoo.com>
2016-06-13 23:16:55 -0500

    chore(carts): Enhance model

--- models/cart.js
+++ models/cart.js
@@ -3,11 +3,16 @@
 let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
+/**
+ * Provides the schema for shopping carts.
+ *
+ * 'status' expected values: READY, CANCELED, PURCHASED, PENDING.
+ */
 let cartSchema = new Schema({
-    created     : { type: Date, default: Date.now },
-    owner       : { type: Schema.Types.ObjectId, ref: 'User' },
-    products    : [{ type: Schema.Types.ObjectId, ref: 'Product' }],
-    status      : { type: String, default: 'READY' } // READY, PURCHASED, CANCELED
+    created_at: { type: Date, default: Date.now },
+    owner     : { type: Schema.Types.ObjectId, ref: 'User' },
+    products  : [{ type: Schema.Types.ObjectId, ref: 'Product' }],
+    status    : { type: String, default: 'READY' }
 }, { collection: 'carts' });
 
 let Cart = mongoose.model('Cart', cartSchema);
